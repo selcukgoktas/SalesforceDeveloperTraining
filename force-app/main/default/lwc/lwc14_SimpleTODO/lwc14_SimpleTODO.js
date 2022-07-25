dd100882692f7170bf73b087a20638547a2912c7
Selçuk Göktaş <developer@pbo.com>
2022-07-25 18:35:34 +0300

    LWC14 independent component communication

---
+++ force-app/main/default/lwc/lwc14_SimpleTODO/lwc14_SimpleTODO.js
@@ -0,0 +1,56 @@
+import { LightningElement,track } from 'lwc';
+
+export default class Lwc14_SimpleTODO extends LightningElement {
+  @track todoList=[];
+  itemnum=0;
+  item;
+ 
+
+  textonchangeHandler(event){
+    this.item=event.target.value;
+  }
+
+  additemtoListHandler(){
+    
+      let val={
+        id:this.itemnum,
+        status:'action:check',
+        item:this.item
+      }
+      
+
+      this.todoList=[       
+        val,
+        ...this.todoList
+      ];
+      this.itemnum+=1;
+
+  }
+
+  clickApproveHandler(event){
+
+    // event.target.dataset.index
+
+    
+
+    let arrIndex=event.target.dataset.index;
+
+    console.log(arrIndex);
+
+    this.todoList[arrIndex].status='action:approval';
+
+    console.log(this.todoList[arrIndex].status);
+
+    console.log(event.target.dataset.index);
+  }
+
+  clickDeleteHandler(event){
+    
+    console.log(event.target.dataset.index);
+    let arrIndex=event.target.dataset.index;
+
+    this.todoList.splice(arrIndex,1);
+
+  }
+
+}
